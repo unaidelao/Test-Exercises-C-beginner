1140c2145726fb2c387455102e7474871a9b62b0
Unai <unai.delao@gmail.com>
2016-12-26 00:25:21 +0100

    Muestra mes corriente, estilo calendario

---
+++ calendario.c
@@ -0,0 +1,17 @@
+/* Simple programa que muestra en pantalla un mes, estilo calendario */
+
+#include <stdio.h>
+
+int main (void)
+
+{
+	printf("L\tM\tX\tJ\tV\tS\tD\n");
+	printf("\t\t\t\t\t\t1\n");
+	printf("2\t3\t4\t5\t6\t7\t8\n");
+	printf("9\t10\t11\t12\t13\t14\t15\n");
+	printf("16\t17\t18\t19\t20\t21\t22\n");
+	printf("23\t24\t25\t26\t27\t28\t29\n");
+	printf("30\t31\t\t\t\t\t");
+	
+	return 0;
+}
