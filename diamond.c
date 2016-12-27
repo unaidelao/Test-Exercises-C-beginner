6037960491d3cb27677624afcd8d8b0103c1fd6c
Unai <unai.delao@gmail.com>
2016-12-27 19:56:05 +0100

    Diamond using printf

---
+++ diamond.c
@@ -0,0 +1,17 @@
+/* Print a diamond using * and \t */
+
+#include <stdio.h>
+
+int main(void)
+
+{
+	printf("\t\t\t*\n");
+	printf("\t\t*\t\t*\t\t\n");
+	printf("\t*\t\t\t\t*\t\n");
+	printf("*\t\t\t\t\t\t*\n");
+	printf("\t*\t\t\t\t*\t\n");
+	printf("\t\t*\t\t*\t\t\n");
+	printf("\t\t\t*\n");
+
+	return 0;
+}
